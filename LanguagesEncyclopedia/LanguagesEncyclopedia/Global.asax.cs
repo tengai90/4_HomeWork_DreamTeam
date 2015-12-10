5d1d85d1bd8c5c4cb8c8b09ac86f1eb2516abad4
tengai90 <z.smulko@siadrs.lv>
2015-12-11 01:24:32 +0200

    DB+connection+bootstrap (fixed)

--- LanguagesEncyclopedia/LanguagesEncyclopedia/Global.asax.cs
+++ LanguagesEncyclopedia/LanguagesEncyclopedia/Global.asax.cs
@@ -22,6 +22,8 @@ namespace LanguagesEncyclopedia
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
+            
         }
     }
 }
